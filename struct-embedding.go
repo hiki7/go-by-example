11b46e048d1f9a08bc138638fa5a2190d564d32d
hiki7 <rsaliyev03@gmail.com>
2025-01-26 15:57:31 +0500

    struct-embedding

---
+++ struct-embedding.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// in Go there are no classes and there are no class inheritance
+// instead of it Go uses embedding - when one structure contains another structure
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
+func main() {
+	co := container{
+		base: base{
+			num: 1,
+		},
+		str: "some name",
+	}
+
+	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
+
+	fmt.Println("also num:", co.base.num)
+
+	fmt.Println("describe:", co.describe())
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co
+	fmt.Println("describer:", d.describe())
+}
