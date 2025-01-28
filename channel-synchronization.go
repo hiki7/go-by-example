de1fc912dcbe5c735a7ad3da7e6fa964036bc4b7
hiki7 <rsaliyev03@gmail.com>
2025-01-28 11:50:24 +0500

    additional comment

--- channel-synchronization.go
+++ channel-synchronization.go
@@ -13,8 +13,9 @@ func worker(done chan bool) {
 	done <- true
 }
 
+// the main function waits for worker to complete before exiting
 func main() {
-	done := make(chan bool, 1) //buffered channel size 1
+	done := make(chan bool, 1) //buffered channel size 1, this channel is used to signal main function when the worker function is done
 	go worker(done)            //running worker in a separate goroutine. This starts worker in parallel, allowing main() to keep running
 
 	<-done
